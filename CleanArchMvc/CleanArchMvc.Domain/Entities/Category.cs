c118cc6af24c2b5f56294d7e0926ef3650187134
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2026-01-18 04:53:36 -0300

    add teste unitário para classes Category e Product

--- CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -3,10 +3,8 @@ using System.Collections;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public sealed class Category
+    public sealed class Category : Entity
     {
-        public int Id { get; private set; }
-
         public string Name { get; private set; }
 
         public Category(string name)
@@ -16,9 +14,14 @@ namespace CleanArchMvc.Domain.Entities
 
         public Category(int id, string name)
         {
-            DomainExceptionValidation.When(id < 0, "Invalid id value");
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
             Id = id;
 
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
             ValidateDomain(name);
         }
 
@@ -30,7 +33,7 @@ namespace CleanArchMvc.Domain.Entities
                 "Invalid name. Name is required");
 
             DomainExceptionValidation.When(name.Length < 3,
-                "Invalid name. too short, minim 3 characters");
+                "Invalid name. too short, minimum 3 characters");
 
             Name = name;
         }
